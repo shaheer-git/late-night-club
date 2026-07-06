7ca86d54e536fdf3c971f3741d52d96e3980fc58
shaheer-git <mshaheer1859@gmail.com>
2026-07-06 19:06:48 +0530

    Add Docker Puppeteer flags to fix crash

--- whatsapp-gateway/index.js
+++ whatsapp-gateway/index.js
@@ -30,7 +30,16 @@ async function startGateway() {
         }),
         puppeteer: {
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox', 
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu'
+            ]
         }
     });
 
